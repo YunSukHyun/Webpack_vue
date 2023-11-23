2814b5aaa1a49c681c2811bf85b650f8e6e442ea
YunSukHyun <czcz9207@gmail.com>
2023-11-23 16:44:38 +0900

    eslint config

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1
+    v-if="msg"
+    class="message"
+    @click="log">
+    {{ msg }}
+  </h1>
+  
   <Hello />
 </template>
 <script>
-import Hello from "~/components/Hello";
+import Hello from '~/components/Hello'
 export default {
   components: {
     Hello,
   },
   data() {
-    return { msg: "Hello Vue.." };
+    return { msg: 'Hello Vue..' }
   },
-};
+}
 </script>
 <style>
 h1 {
