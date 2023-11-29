87e79a1baa74827c020afceda4b6c11d703dbf8c
YunSukHyun <czcz9207@gmail.com>
2023-11-29 12:19:45 +0900

    feat: add store

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,6 @@
 <template>
-  <h1
-    v-if="msg"
-    class="message"
-    @click="log">
-    {{ msg }}
-  </h1>
-  
-  <Hello />
+  <RouterView />
 </template>
 <script>
-import Hello from '~/components/Hello'
-export default {
-  components: {
-    Hello,
-  },
-  data() {
-    return { msg: 'Hello Vue..' }
-  },
-}
 </script>
 <style>
-h1 {
-  color: red;
-}
-</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,47 @@
-const path = require('path')
-const { VueLoaderPlugin } = require('vue-loader')
-const HtmlPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   resolve: {
-    extensions: ['.vue', '.js'], // vue, js 확장자를 생략하겠다.
+    extensions: [".vue", ".js"], // vue, js 확장자를 생략하겠다.
     alias: {
-      '~': path.resolve(__dirname, 'src'),
+      "~": path.resolve(__dirname, "src"),
     },
   },
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.vue$/, // .vue로 끝나는 확장자만
-        use: 'vue-loader', // 해석 도움
+        use: "vue-loader", // 해석 도움
       },
       {
         test: /\.s?css$/,
         use: [
           // 순서가 중요 먼저 로드가 되야하는 걸 뒤에
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader',
+          "vue-style-loader",
+          "css-loader",
+          "sass-loader",
         ],
       },
     ],
   },
   plugins: [
     new VueLoaderPlugin(),
-    new HtmlPlugin({ template: './src/index.html' }),
+    new HtmlPlugin({ template: "./src/index.html" }),
     new CopyPlugin({
-      patterns: [{ from: 'static' }],
+      patterns: [{ from: "static" }],
     }),
   ],
-}
+  devServer: {
+    historyApiFallback: true,
+  },
+};
